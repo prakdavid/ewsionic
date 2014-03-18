d817f0a9a7ccaa290b6353b910b3387183016a26
David PRAK <prak_d@etna-alternance.net>
2014-03-18 20:13:19 +0100

    Répare le bouton leftbutton du header

--- www/js/controllers/mainCtrl.js
+++ www/js/controllers/mainCtrl.js
@@ -1,4 +1,10 @@
 angular.module('ews')
-    .controller('MainCtrl', function($scope, $state) {
+    .controller('MainCtrl', function($scope, $state, $ionicSideMenuDelegate) {
         console.log("MainCtrl");
+        $scope.leftButtons = [{
+          type: 'button-icon button-clear ion-navicon',
+          tap: function(e) {
+            $ionicSideMenuDelegate.toggleLeft($scope.$$childHead);
+          }
+        }];
 });
